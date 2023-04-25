304b2a18f12b1f3167a2304075fe777101078e23
lisa-lumos <zjcty307@gmail.com>
2023-04-25 12:10:59 -0600

    add streamlit api calls and dashboard

--- 02-streamlit/01-intro.md
+++ 02-streamlit/01-intro.md
@@ -539,35 +539,149 @@ Whenever a value in pounds input box changes, the corresponding session var `lbs
 
 <img src="images/19-session-vars.png">
 
-## 
+## Use API within a Streamlit app
+```py
+import streamlit as st
+import requests
 
+st.title('🏀 Bored API app')
 
+st.sidebar.header('Input')
+selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
 
+suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+json_data = requests.get(suggested_activity_url)
+suggested_activity = json_data.json()
 
-## 
+c1, c2 = st.columns(2)
+with c1:
+  with st.expander('About this app'):
+    st.write('Are you bored? The **Bored API app** provides suggestions on activities that you can do when you are bored. This app is powered by the Bored API.')
+with c2:
+  with st.expander('JSON data'):
+    st.write(suggested_activity)
 
+st.header('Suggested activity')
+st.info(suggested_activity['activity'])
 
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.metric(label='Number of Participants', value=suggested_activity['participants'], delta='')
+with col2:
+  st.metric(label='Type of Activity', value=suggested_activity['type'].capitalize(), delta='')
+with col3:
+  st.metric(label='Price', value=suggested_activity['price'], delta='')
+```
 
+<img src="images/20-api.png">
 
-## 
+## Draggable and resizable dashboard
+Streamlit Elements is a third-party component made by okld that gives you the tools to compose beautiful applications and dashboards with Material UI widgets, Monaco editor (Visual Studio Code), Nivo charts, and more.
+```py
+import json
+import streamlit as st
+from pathlib import Path
+from streamlit_elements import elements, dashboard, mui, editor, media, lazy, sync, nivo # pip install streamlit-elements
 
+st.set_page_config(layout="wide")
 
+with st.sidebar:
+    st.title("🗓️ #30DaysOfStreamlit")
+    st.header("Day 27 - Streamlit Elements")
+    st.write("Build a draggable and resizable dashboard with Streamlit Elements.")
+    st.write("---")
 
+    # URL for media player.
+    media_url = st.text_input("Media URL", value="https://www.youtube.com/watch?v=vIQQR_yq-8I")
 
-## 
+# data for code editor and chart, session state item will be updated when code changes
+if "data" not in st.session_state:
+    st.session_state.data = Path("data.json").read_text()
 
+# Define a default dashboard layout. Dashboard grid has 12 columns by default.
+layout = [
+    # Editor item at x=0 and y=0, takes 6/12 columns and has a height of 3.
+    dashboard.Item("editor", 0, 0, 6, 3),
+    # Chart item at x=6 and y=0, takes 6/12 columns and has a height of 3.
+    dashboard.Item("chart", 6, 0, 6, 3),
+    # Media item at x=0 and y=3, takes 6/12 columns and has a height of 4.
+    dashboard.Item("media", 0, 2, 12, 4),
+]
 
+with elements("demo"): # Create a frame to display elements.
+    # a new dashboard with the specified layout
+    with dashboard.Grid(layout, draggableHandle=".draggable"): # CSS query selector to define the draggable part of each dashboard item.
 
+        # 1. the code editor
+        # To make card's content automatically fill the height available, use CSS flexbox.
+        # sx defines CSS attributes.
+        with mui.Card(key="editor", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="Editor", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+                editor.Monaco(
+                    defaultValue=st.session_state.data,
+                    language="json",
+                    onChange=lazy(sync("data"))
+                )
 
-## 
+            with mui.CardActions:
+                mui.Button("Apply changes", onClick=sync())
 
+        # 2. the Nivo Bump chart
+        with mui.Card(key="chart", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="Chart", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+                nivo.Bump(
+                    data=json.loads(st.session_state.data),
+                    colors={ "scheme": "spectral" },
+                    lineWidth=3,
+                    activeLineWidth=6,
+                    inactiveLineWidth=3,
+                    inactiveOpacity=0.15,
+                    pointSize=10,
+                    activePointSize=16,
+                    inactivePointSize=0,
+                    pointColor={ "theme": "background" },
+                    pointBorderWidth=3,
+                    activePointBorderWidth=3,
+                    pointBorderColor={ "from": "serie.color" },
+                    axisTop={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "",
+                        "legendPosition": "middle",
+                        "legendOffset": -36
+                    },
+                    axisBottom={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "",
+                        "legendPosition": "middle",
+                        "legendOffset": 32
+                    },
+                    axisLeft={
+                        "tickSize": 5,
+                        "tickPadding": 5,
+                        "tickRotation": 0,
+                        "legend": "ranking",
+                        "legendPosition": "middle",
+                        "legendOffset": -40
+                    },
+                    margin={ "top": 40, "right": 100, "bottom": 40, "left": 60 },
+                    axisRight=None,
+                )
 
+        # 3. the Media player
+        with mui.Card(key="media", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="Media Player", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+                media.Player(url=media_url, width="100%", height="100%", controls=True)
+```
 
-
-## 
-
-
-
+## streamlit-shap
+streamlit-shap is a Streamlit component that provides a wrapper to display SHAP plots in Streamlit.
 
 ## my notes
 - Clicking a button will not cause the page to reload, instead, its status is set to "got clicked"
--- 02-streamlit/streamlit_app.py
+++ 02-streamlit/streamlit_app.py
@@ -311,29 +311,156 @@ import streamlit as st
 # b1 = time()
 # st.info(b1-b0)
 
-import streamlit as st
-st.title('st.session_state')
+# import streamlit as st
+# st.title('st.session_state')
 
-def lbs_to_kg():
-  st.session_state.kg = st.session_state.lbs/2.2046
-def kg_to_lbs():
-  st.session_state.lbs = st.session_state.kg*2.2046
+# def lbs_to_kg():
+#   st.session_state.kg = st.session_state.lbs/2.2046
+# def kg_to_lbs():
+#   st.session_state.lbs = st.session_state.kg*2.2046
 
-st.header('Input')
-col1, spacer, col2 = st.columns([2,1,2])
-with col1:
-  pounds = st.number_input("Pounds:", key = "lbs", on_change = lbs_to_kg) # with key "lbs", st.session_state.lbs now exists!
-with col2:
-  kilogram = st.number_input("Kilograms:", key = "kg", on_change = kg_to_lbs) # with key "kg", st.session_state.kg now exists!
+# st.header('Input')
+# col1, spacer, col2 = st.columns([2,1,2])
+# with col1:
+#   pounds = st.number_input("Pounds:", key = "lbs", on_change = lbs_to_kg) # with key "lbs", st.session_state.lbs now exists!
+# with col2:
+#   kilogram = st.number_input("Kilograms:", key = "kg", on_change = kg_to_lbs) # with key "kg", st.session_state.kg now exists!
 
-st.header('Output')
-st.write("st.session_state object:", st.session_state)
+# st.header('Output')
+# st.write("st.session_state object:", st.session_state)
+
+# import streamlit as st
+# import requests
+
+# st.title('🏀 Bored API app')
+
+# st.sidebar.header('Input')
+# selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
+
+# suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+# json_data = requests.get(suggested_activity_url)
+# suggested_activity = json_data.json()
+
+# c1, c2 = st.columns(2)
+# with c1:
+#   with st.expander('About this app'):
+#     st.write('Are you bored? The **Bored API app** provides suggestions on activities that you can do when you are bored. This app is powered by the Bored API.')
+# with c2:
+#   with st.expander('JSON data'):
+#     st.write(suggested_activity)
+
+# st.header('Suggested activity')
+# st.info(suggested_activity['activity'])
+
+# col1, col2, col3 = st.columns(3)
+# with col1:
+#   st.metric(label='Number of Participants', value=suggested_activity['participants'], delta='')
+# with col2:
+#   st.metric(label='Type of Activity', value=suggested_activity['type'].capitalize(), delta='')
+# with col3:
+#   st.metric(label='Price', value=suggested_activity['price'], delta='')
 
 
+# import json
+# import streamlit as st
+# from pathlib import Path
+# from streamlit_elements import elements, dashboard, mui, editor, media, lazy, sync, nivo # pip install streamlit-elements
 
+# st.set_page_config(layout="wide")
 
+# with st.sidebar:
+#     st.title("🗓️ #30DaysOfStreamlit")
+#     st.header("Day 27 - Streamlit Elements")
+#     st.write("Build a draggable and resizable dashboard with Streamlit Elements.")
+#     st.write("---")
 
+#     # URL for media player.
+#     media_url = st.text_input("Media URL", value="https://www.youtube.com/watch?v=vIQQR_yq-8I")
 
+# # data for code editor and chart, session state item will be updated when code changes
+# if "data" not in st.session_state:
+#     st.session_state.data = Path("data.json").read_text()
+
+# # Define a default dashboard layout. Dashboard grid has 12 columns by default.
+# layout = [
+#     # Editor item at x=0 and y=0, takes 6/12 columns and has a height of 3.
+#     dashboard.Item("editor", 0, 0, 6, 3),
+#     # Chart item at x=6 and y=0, takes 6/12 columns and has a height of 3.
+#     dashboard.Item("chart", 6, 0, 6, 3),
+#     # Media item at x=0 and y=3, takes 6/12 columns and has a height of 4.
+#     dashboard.Item("media", 0, 2, 12, 4),
+# ]
+
+# with elements("demo"): # Create a frame to display elements.
+#     # a new dashboard with the specified layout
+#     with dashboard.Grid(layout, draggableHandle=".draggable"): # CSS query selector to define the draggable part of each dashboard item.
+
+#         # 1. the code editor
+#         # To make card's content automatically fill the height available, use CSS flexbox.
+#         # sx defines CSS attributes.
+#         with mui.Card(key="editor", sx={"display": "flex", "flexDirection": "column"}):
+#             mui.CardHeader(title="Editor", className="draggable")
+#             with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+#                 editor.Monaco(
+#                     defaultValue=st.session_state.data,
+#                     language="json",
+#                     onChange=lazy(sync("data"))
+#                 )
+
+#             with mui.CardActions:
+#                 mui.Button("Apply changes", onClick=sync())
+
+#         # 2. the Nivo Bump chart
+#         with mui.Card(key="chart", sx={"display": "flex", "flexDirection": "column"}):
+#             mui.CardHeader(title="Chart", className="draggable")
+#             with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+#                 nivo.Bump(
+#                     data=json.loads(st.session_state.data),
+#                     colors={ "scheme": "spectral" },
+#                     lineWidth=3,
+#                     activeLineWidth=6,
+#                     inactiveLineWidth=3,
+#                     inactiveOpacity=0.15,
+#                     pointSize=10,
+#                     activePointSize=16,
+#                     inactivePointSize=0,
+#                     pointColor={ "theme": "background" },
+#                     pointBorderWidth=3,
+#                     activePointBorderWidth=3,
+#                     pointBorderColor={ "from": "serie.color" },
+#                     axisTop={
+#                         "tickSize": 5,
+#                         "tickPadding": 5,
+#                         "tickRotation": 0,
+#                         "legend": "",
+#                         "legendPosition": "middle",
+#                         "legendOffset": -36
+#                     },
+#                     axisBottom={
+#                         "tickSize": 5,
+#                         "tickPadding": 5,
+#                         "tickRotation": 0,
+#                         "legend": "",
+#                         "legendPosition": "middle",
+#                         "legendOffset": 32
+#                     },
+#                     axisLeft={
+#                         "tickSize": 5,
+#                         "tickPadding": 5,
+#                         "tickRotation": 0,
+#                         "legend": "ranking",
+#                         "legendPosition": "middle",
+#                         "legendOffset": -40
+#                     },
+#                     margin={ "top": 40, "right": 100, "bottom": 40, "left": 60 },
+#                     axisRight=None,
+#                 )
+
+#         # 3. the Media player
+#         with mui.Card(key="media", sx={"display": "flex", "flexDirection": "column"}):
+#             mui.CardHeader(title="Media Player", className="draggable")
+#             with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+#                 media.Player(url=media_url, width="100%", height="100%", controls=True)
 
 
 
